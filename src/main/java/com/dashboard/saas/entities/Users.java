a22f6b41cec34c9f8fc55b5e607b66cd74207d16
Suraj.Yadav <yadav.suraj@fyntune.com>
2026-05-19 12:08:08 +0530

    fix(feat/sasa-based-projects):Fixed Import Pom.xml

---
+++ src/main/java/com/dashboard/saas/entities/Users.java
@@ -0,0 +1,90 @@
+package com.dashboard.saas.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String mobile;
+
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+    public Users() {
+    }
+
+    public Users(Long id, String name, String email, String mobile, String password, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/com/dashboard/saas/repositories/UserRepository.java
+++ src/main/java/com/dashboard/saas/repositories/UserRepository.java
@@ -1,4 +1,15 @@
 package com.dashboard.saas.repositories;
 
-public interface UserRepository {
+import com.dashboard.saas.entities.Users;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository  extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByEmail(String email);
 }
